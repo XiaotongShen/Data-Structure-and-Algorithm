38c30e81cbbeb479c24c1b1787a99b411e3f4106
XiaotongShen <xiaotong.shenn@outlook.com>
2022-08-31 17:48:05 +0800

    update

--- Zuo/Basic/Class03/Code06_TwoStackImplementQueue.py
+++ Zuo/Basic/Class03/Code06_TwoStackImplementQueue.py
@@ -8,3 +8,33 @@ Author:
 Date: 
     2022/8/31
 """
+
+
+class Stack:
+
+    def __init__(self, items=None):
+        self.items = items
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, val: int):
+        self.items.append(val)
+
+    def pop(self):
+        if self.is_empty():
+            print("Your Stack is Empty.")
+        else:
+            return self.items.pop()
+
+    def peek(self):
+        if self.is_empty():
+            print("Your Stack is Empty.")
+        else:
+            return self.items[-1]
+
+    def travel(self):
+        if self.is_empty():
+            print("Your Stack is Empty.")
+        else:
+            print(self.items)
