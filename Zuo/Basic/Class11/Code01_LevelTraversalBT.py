97413bc6f0483cf089402ccd9787b4ff2224145a
XiaotongShen <xiaotong.shenn@outlook.com>
2022-10-21 21:54:07 +0800

    update

--- Zuo/Basic/Class11/Code01_LevelTraversalBT.py
+++ Zuo/Basic/Class11/Code01_LevelTraversalBT.py
@@ -5,3 +5,40 @@
 @Author: Sarah Shen
 @Time: 21/10/2022 15:50
 """
+
+from queue import Queue
+
+
+class Node:
+
+    def __init__(self, v: int):
+        self.value = v
+        self.left = None
+        self.right = None
+
+
+def level(head):
+    if head is None:
+        return
+    q = Queue()
+    q.put(head)
+    while not q.empty():
+        cur = q.get()
+        print(cur.value)
+        if cur.left is not None:
+            q.put(cur.left)
+        if cur.right is not None:
+            q.put(cur.right)
+
+
+if __name__ == "__main__":
+    h = Node(1)
+    h.left = Node(2)
+    h.right = Node(3)
+    h.left.left = Node(4)
+    h.left.right = Node(5)
+    h.right.left = Node(6)
+    h.right.right = Node(7)
+
+    level(h)
+    print("========")
--- Zuo/Basic/Class11/Code02_SerializeAndReconstructTree.py
+++ Zuo/Basic/Class11/Code02_SerializeAndReconstructTree.py
@@ -5,3 +5,11 @@
 @Author: Sarah Shen
 @Time: 21/10/2022 15:50
 """
+
+
+class Node:
+
+    def __init__(self, v: int):
+        self.value = v
+        self.left = None
+        self.right = None
