445366fdc221062f96c60046d93e39100bc2253d
XiaotongShen <xiaotong.shenn@outlook.com>
2022-10-26 19:32:32 +0800

    update

--- Zuo/Basic/Class17/Code05_ReversStackUsingRecursive.py
+++ Zuo/Basic/Class17/Code05_ReversStackUsingRecursive.py
@@ -5,4 +5,28 @@
 @Author: Sarah Shen
 @Time: 26/10/2022 10:04
 """
-# TODO:
+
+
+def reverse(stack):
+    if stack is None or len(stack) == 0:
+        return
+    i = f(stack)
+    reverse(stack)
+    stack.append(i)
+
+
+def f(stack):
+    res = stack.pop()
+    if len(stack) == 0:
+        return res
+    else:
+        last = f(stack)
+        stack.append(res)
+        return last
+
+
+if __name__ == '__main__':
+    test = [1, 2, 3, 4, 5, 6]
+    reverse(test)
+    while len(test) != 0:
+        print(test.pop())
