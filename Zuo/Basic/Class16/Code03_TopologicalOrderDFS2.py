6b3b4801414f30f9e503f0a12a37bf149e851263
XiaotongShen <xiaotong.shenn@outlook.com>
2022-10-26 11:59:42 +0800

    update

--- Zuo/Basic/Class16/Code03_TopologicalOrderDFS2.py
+++ Zuo/Basic/Class16/Code03_TopologicalOrderDFS2.py
@@ -5,4 +5,86 @@
 @Author: Sarah Shen
 @Time: 25/10/2022 17:12
 """
-# TODO
+
+
+# OJ链接：https://www.lintcode.com/problem/topological-sorting
+
+
+class DirectedGraphNode:
+
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = list()
+
+
+class Record:
+
+    def __init__(self, n: DirectedGraphNode, o: int):
+        self.node = n  # 节点本身
+        self.nodes = o  # 节点往下走所有的点次
+
+
+def comparator(o1, o2):
+    return 0 if o1.nodes == o2.nodes else (o2.nodes - o1.nodes)
+
+
+def cmp_to_key(mycmp):
+    """
+    Convert a cmp= function into a key= function
+    """
+
+    class K:
+        def __init__(self, obj):
+            self.obj = obj
+
+        def __lt__(self, other):
+            return mycmp(self.obj, other.obj) < 0
+
+        def __gt__(self, other):
+            return mycmp(self.obj, other.obj) > 0
+
+        def __eq__(self, other):
+            return mycmp(self.obj, other.obj) == 0
+
+        def __le__(self, other):
+            return mycmp(self.obj, other.obj) <= 0
+
+        def __ge__(self, other):
+            return mycmp(self.obj, other.obj) >= 0
+
+        def __ne__(self, other):
+            return mycmp(self.obj, other.obj) != 0
+
+    return K
+
+
+def top_sort(graph):
+    order = dict()
+    for cur in graph:
+        f(cur, order)
+    record_arr = list()
+    for r in order.values():
+        record_arr.append(r)
+    record_arr.sort(key=cmp_to_key(comparator))
+    ans = list()
+    for r in record_arr:
+        ans.append(r.node)
+    return ans
+
+
+# 当前来到cur点，请返回cur点所到之处，所有的点次
+# 返回（cur, 点次）
+# 缓存入order
+# key： 某一个点的点次，之前算过了
+# value：点次是多少
+def f(cur: DirectedGraphNode, order: dict):
+    # cur的点次之前算过
+    if order.keys().__contains__(cur):
+        return order[cur]
+    # cur的点次之前没算过
+    nodes = 0
+    for nxt in cur.neighbors:
+        nodes += f(nxt, order).nodes
+    ans = Record(cur, nodes + 1)
+    order[cur] = ans
+    return ans
--- Zuo/Basic/Class16/Code03_TopologySort.py
+++ Zuo/Basic/Class16/Code03_TopologySort.py
@@ -5,4 +5,26 @@
 @Author: Sarah Shen
 @Time: 25/10/2022 17:12
 """
-# TODO
+# 拓扑排序不唯一，但是每一个都对
+# 思路：使用入do，入do为零的打印，消除影响
+from queue import Queue
+
+
+def sorted_topology(graph):
+    # key是某个节点，value为剩余的入度
+    in_map = dict()
+    # 只有剩余入度是0的节点，才进这个队列
+    zero_in_queue = Queue()
+    for node in graph.nodes:
+        in_map[node] = node.in_nodes
+        if node.in_nodes == 0:
+            zero_in_queue.put(node)
+    res = list()
+    while not zero_in_queue.empty():
+        cur = zero_in_queue.get()
+        res.append(cur)
+        for nxt in cur.next_nodes:
+            in_map[nxt] = in_map.get(nxt) - 1
+            if in_map.get(nxt) == 0:
+                zero_in_queue.put(nxt)
+    return res
