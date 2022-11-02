ae987f11f3587d3482268c6039f7438c5af43572
XiaotongShen <xiaotong.shenn@outlook.com>
2022-11-02 17:02:53 +0800

    update

--- Zuo/Basic/Class25/Code03_LargestRectangleInHistogram.py
+++ Zuo/Basic/Class25/Code03_LargestRectangleInHistogram.py
@@ -5,3 +5,31 @@
 @Author: Sarah Shen
 @Time: 31/10/2022 22:47
 """
+
+
+# 测试链接：https://leetcode.com/problems/largest-rectangle-in-histogram
+
+class Solution:
+
+    # def largestRectangleArea(self, heights: List[int]) -> int:
+    @staticmethod
+    def largest_rectangle_area1(heights: [int]):
+        if heights is None or len(heights) == 0:
+            return 0
+        max_area = 0
+        s = list()
+        for i in range(len(heights)):
+            while len(s) != 0 and heights[s[-1]] >= heights[i]:
+                j = s.pop()
+                k = -1 if len(s) == 0 else s[-1]
+                cur_area = (i - k - 1) * heights[j]
+                max_area = max(max_area, cur_area)
+            s.append(i)
+        while len(s) != 0:
+            j = s.pop()
+            k = -1 if len(s) == 0 else s[-1]
+            cur_area = (len(heights) - k - 1) * heights[j]
+            max_area = max(max_area, cur_area)
+        return max_area
+
+
