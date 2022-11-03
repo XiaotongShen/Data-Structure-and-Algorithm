75333410661e28331d2a53a31b966a8429593f02
XiaotongShen <xiaotong.shenn@outlook.com>
2022-11-03 12:47:51 +0800

    update

--- Zuo/Basic/Class26/Code01_SumOfSubarrayMinimums.py
+++ Zuo/Basic/Class26/Code01_SumOfSubarrayMinimums.py
@@ -5,4 +5,172 @@
 @Author: Sarah Shen
 @Time: 02/11/2022 17:37
 """
-# TODO
+import random
+
+
+# 测试链接：https://leetcode.com/problems/sum-of-subarray-minimums/
+# subArrayMinSum1是暴力解
+# subArrayMinSum2是最优解的思路
+# sumSubarrayMin是最优解思路下的单调栈优化
+# Leetcode上不要提交subArrayMinSum1、subArrayMinSum2方法，因为没有考虑取摸
+# Leetcode上只提交sumSubarrayMin方法，时间复杂度O(N)，可以直接通过
+def sum_array_min_sum1(arr: [int]):
+    ans = 0
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            min_value = arr[i]
+            for k in range(i + 1, j + 1):
+                min_value = min(min_value, arr[k])
+            ans += min_value
+    return ans
+
+
+def sub_array_min_sum2(arr: [int]):
+    # left[i] = x: arr[i]左边，离arr[i]最近，<=arr[i],位置在x
+    left = left_near_less_equal2(arr)
+    # right[i] =y: arr[i]右边，离arr[i]最近，< arr[i],位置在y
+    right = right_near_less2(arr)
+    ans = 0
+    for i in range(len(arr)):
+        start = i - left[i]
+        end = right[i] - i
+        ans += start * end * arr[i]
+    return ans
+
+
+def left_near_less_equal2(arr: [int]):
+    n = len(arr)
+    left = [0] * n
+    for i in range(n):
+        ans = -1
+        for j in range(i - 1, -1, -1):
+            if arr[j] <= arr[i]:
+                ans = j
+                break
+        left[i] = ans
+    return left
+
+
+def right_near_less2(arr: [int]):
+    n = len(arr)
+    right = [0] * n
+    for i in range(n):
+        ans = n
+        for j in range(i + 1, n):
+            if arr[i] > arr[j]:
+                ans = j
+                break
+        right[i] = ans
+    return right
+
+
+def sum_sub_array_min(arr: [int]):
+    stack = []
+    left = near_less_equal_left(arr, stack)
+    right = near_less_right(arr, stack)
+    ans = 0
+    for i in range(len(arr)):
+        start = i - left[i]
+        end = right[i] - i
+        ans += start * end * arr[i]
+        ans %= 1000000007
+    return ans
+
+
+def near_less_equal_left(arr: [int], stack: [int]):
+    n = len(arr)
+    left = [0] * n
+    for i in range(n - 1, -1, -1):
+        while len(stack) != 0 and arr[stack[-1]] >= arr[i]:
+            j = stack.pop()
+            left[j] = i
+        stack.append(i)
+    while len(stack) != 0:
+        j = stack.pop()
+        left[j] = -1
+    return left
+
+
+def near_less_right(arr: [int], stack: [int]):
+    n = len(arr)
+    right = [0] * n
+    for i in range(n):
+        while len(stack) != 0 and arr[stack[-1]] > arr[i]:
+            j = stack.pop()
+            right[j] = i
+        stack.append(i)
+    while len(stack) != 0:
+        j = stack.pop()
+        right[j] = n
+    return right
+
+
+class Solution:
+
+    # def sumSubarrayMins(self, arr: List[int]) -> int:
+    def sum_sub_array_min(self, arr: [int]):
+        stack = []
+        left = self.near_less_equal_left(arr, stack)
+        right = self.near_less_right(arr, stack)
+        ans = 0
+        for i in range(len(arr)):
+            start = i - left[i]
+            end = right[i] - i
+            ans += start * end * arr[i]
+            ans %= 1000000007
+        return ans
+
+    @staticmethod
+    def near_less_equal_left(arr: [int], stack: [int]):
+        n = len(arr)
+        left = [0] * n
+        for i in range(n - 1, -1, -1):
+            while len(stack) != 0 and arr[stack[-1]] >= arr[i]:
+                j = stack.pop()
+                left[j] = i
+            stack.append(i)
+        while len(stack) != 0:
+            j = stack.pop()
+            left[j] = -1
+        return left
+
+    @staticmethod
+    def near_less_right(arr: [int], stack: [int]):
+        n = len(arr)
+        right = [0] * n
+        for i in range(n):
+            while len(stack) != 0 and arr[stack[-1]] > arr[i]:
+                j = stack.pop()
+                right[j] = i
+            stack.append(i)
+        while len(stack) != 0:
+            j = stack.pop()
+            right[j] = n
+        return right
+
+
+def random_array(max_l: int, max_v: int):
+    n = int(random.random() * max_l)
+    ans = [0] * n
+    for i in range(n):
+        ans[i] = int(random.random() * max_v) + 1
+    return ans
+
+
+if __name__ == '__main__':
+    max_len = 100
+    max_value = 50
+    test_times = 1000
+    print('测试开始')
+    for i in range(test_times):
+        r_arr = random_array(max_len, max_value)
+        ans1 = sum_array_min_sum1(r_arr)
+        ans2 = sub_array_min_sum2(r_arr)
+        ans3 = sum_sub_array_min(r_arr)
+        if ans1 != ans2 or ans1 != ans3:
+            print('Oops!')
+            print(r_arr)
+            print(ans1, ans2, ans2)
+            break
+    print('测试结束')
+
--- Zuo/Basic/Class26/Code02_FibonacciProblem.py
+++ Zuo/Basic/Class26/Code02_FibonacciProblem.py
@@ -5,4 +5,154 @@
 @Author: Sarah Shen
 @Time: 02/11/2022 17:37
 """
-# TODO
+
+
+def f1(n: int):
+    """ 暴力递归的方法 """
+    if n < 1:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    return f1(n - 1) + f1(n - 2)
+
+
+def f2(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    res = 1
+    pre = 1
+    tmp = 0
+    for i in range(3, n + 1):
+        temp = res
+        res = res + pre
+        pre = temp
+    return res
+
+
+# O(logN)
+def f3(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    base = [[1, 1], [1, 0]]
+    res = matrix_power(base, n - 2)
+    return res[0][0] + res[1][0]
+
+
+def matrix_power(m: [[int]], p: int):
+    res = [[0] * len(m[0]) for i in range(len(m))]
+    for i in range(len(res)):
+        res[i][i] = 1
+        # res = 矩阵中的1
+    t = m  # 矩阵的1次方
+    while p != 0:
+        if p & 1 != 0:
+            res = product(res, t)
+        t = product(t, t)
+        p >>= 1
+    return res
+
+
+def product(a: [[int]], b: [[int]]):
+    """ 两个矩阵乘完之后的结果返回 """
+    n = len(a)
+    m = len(b[0])
+    k = len(a[0])
+    ans = [[0] * m for i in range(m)]
+    for i in range(n):
+        for j in range(m):
+            for c in range(k):
+                ans[i][j] += a[i][c] * b[c][j]
+    return ans
+
+
+def s1(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2:
+        return n
+    return s1(n - 1) + s1(n - 2)
+
+
+def s2(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2:
+        return n
+    res = 2
+    pre = 1
+    tmp = 0
+    for i in range(3, n + 1):
+        tmp = res
+        res = res + pre
+        pre = tmp
+    return res
+
+
+def s3(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2:
+        return n
+    base = [[1, 1], [1, 0]]
+    res = matrix_power(base, n - 2)
+    return 2 * res[0][0] + res[1][0]
+
+
+def c1(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2 or n == 3:
+        return n
+    return c1(n - 1) + c1(n - 3)
+
+
+def c2(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2 or n == 3:
+        return n
+    res = 3
+    pre = 2
+    pre_pre = 1
+    tmp1 = 0
+    tmp2 = 0
+    for i in range(4, n + 1):
+        tmp1 = res
+        tmp2 = pre
+        res = res + pre_pre
+        pre = tmp1
+        pre_pre = tmp2
+    return res
+
+
+def c3(n: int):
+    if n < 1:
+        return 0
+    if n == 1 or n == 2 or n == 3:
+        return n
+    base = [[1, 1, 0], [0, 0, 1], [1, 0, 0]]
+    res = matrix_power(base, n - 3)
+    return 3 * res[0][0] + 2 * res[1][0] + res[2][0]
+
+
+if __name__ == '__main__':
+    n = 19
+    print(f1(n))
+    print(f2(n))
+    print(f3(n))
+    print("===========")
+
+    print(s1(n))
+    print(s2(n))
+    print(s3(n))
+    print("===========")
+
+    print(c1(n))
+    print(c2(n))
+    print(c3(n))
+    print("===========")
+
