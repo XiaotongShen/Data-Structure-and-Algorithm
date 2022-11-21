d06eaa241aee67ae63d2b00120afa1a279c5b09b
XiaotongShen <xiaotong.shenn@outlook.com>
2022-11-21 13:23:25 +0800

    update

---
+++ Zuo/Basic/Class 31/Code01_SegmentTree.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+@File: Code01_SegmentTree.py
+@Author: Sarah Shen
+@Time: 08/11/2022 18:22
+"""
---
+++ Zuo/Basic/Class 31/Code02_FallingSquares.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""
+@File: Code02_FallingSquares.py
+@Author: Sarah Shen
+@Time: 08/11/2022 18:23
+"""
